8a64c58ae1b63ad8694dc7128ecd16bdc917cc03
ahamedhridoy <ahamed674464@gmail.com>
2024-05-25 17:50:26 +0600

    added button colour

--- ticket.js
+++ ticket.js
@@ -12,8 +12,11 @@ for (const btn of allbtn){
     btn.addEventListener("click",function(e){
        count = count + 1;
          document.getElementById("cnt").innerText = count;
-
+        
        const placename = e.target.parentNode.childNodes[1].innerText;
+      //  const placename = e.target.parentNode.childNodes[1].innerText;
+      //  const placename = e.target.parentNode.childNodes[1].innerText;
+      //  const placename = e.target.parentNode.childNodes[1].innerText;
        const Class = e.target.parentNode.childNodes[9].innerText;
        const Price = e.target.parentNode.childNodes[11].innerText;
           const selectedcontainer = document.getElementById("spl");
@@ -43,6 +46,9 @@ for (const btn of allbtn){
 
           const convterTotalCost = parseInt(gtotalCost)
           document.getElementById("Grandtotal").innerText = convterTotalCost+parseInt(Price);
+           
+
+          const budget = document.getElementById("total-cost").innerText;
 
 
         // console.log(e.target.parentNode.childNodes[].innerText);
@@ -53,12 +59,49 @@ for (const btn of allbtn){
 function setInnerText(id,value){
     document.getElementById(id).innerText = value;
 }
-// let counnt = 8;
+
+ 
+
+const counnt = document.getElementById("sit").innerText;
+const convertcounnt = parseInt(counnt);
+//count =parseInt(counnt) - parseInt("cnt") ;
+//document.getElementById("sit").innerText =parseInt(counnt) - document.getElementById("cnt");
+//console.log(typeof convertcounnt);
 // for (const btn of allbtn){
 //     btn.addEventListener("click",function(e){
-//        count = counnt - 1;
+//        count = parseInt(counnt) - 1;
 //          document.getElementById("sit").innerText = count;
         
         
 //     });
 // }
+document.addEventListener('DOMContentLoaded', (event) => {
+  const button = document.getElementById('mb');
+
+  button.addEventListener('click', () => {
+      button.style.backgroundColor = 'green'; // Change this to the desired color
+  });
+});
+document.addEventListener('DOMContentLoaded', (event) => {
+  const button = document.getElementById('mb-1');
+
+  button.addEventListener('click', () => {
+      button.style.backgroundColor = 'green'; // Change this to the desired color
+  });
+});
+document.addEventListener('DOMContentLoaded', (event) => {
+  const button = document.getElementById('mb-2');
+
+  button.addEventListener('click', () => {
+      button.style.backgroundColor = 'green'; // Change this to the desired color
+  });
+});
+document.addEventListener('DOMContentLoaded', (event) => {
+  const button = document.getElementById('mb-3');
+
+  button.addEventListener('click', () => {
+      button.style.backgroundColor = 'green'; // Change this to the desired color
+  });
+});
+
+
